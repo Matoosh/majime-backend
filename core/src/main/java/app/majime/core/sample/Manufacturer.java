6265e96fd802cd05fa1b56717b2bf25c29fc359e
Maciek <s17245@pjwstk.edu.pl>
2021-01-29 18:40:37 +0100

    Adding manufacturer entity.

---
+++ core/src/main/java/app/majime/core/sample/Manufacturer.java
@@ -0,0 +1,30 @@
+package app.majime.core.sample;
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "manufacturer")
+@Getter
+@Setter
+@AllArgsConstructor
+//@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString
+
+public class Manufacturer {
+
+    @Id
+    @SequenceGenerator(name="manufacturer_seq", sequenceName="manufacturer_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "manufacturer_seq")
+    private Long id;
+
+    @NonNull
+    @Column(name = "manufacturer_no")
+    private String name;
+    private char deleted;
+    private Long address_id;
+
+    protected Manufacturer() {
+
+    }
+}
