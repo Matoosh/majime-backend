4b1c13a686ddf0d30290a2a9fc7f02c4b9d010cb
Maciek <s17245@pjwstk.edu.pl>
2021-02-01 23:21:36 +0100

    Add Swagger

--- infrastructure/src/main/java/app/majime/infrastructure/lims/LimsApplication.java
+++ infrastructure/src/main/java/app/majime/infrastructure/lims/LimsApplication.java
@@ -3,15 +3,15 @@ package app.majime.infrastructure.lims;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.persistence.Entity;
 
 @SpringBootApplication
 @EntityScan("app.majime")
+@EnableSwagger2
 public class LimsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LimsApplication.class, args);
     }
-
 }
