985abba9f4d6f8b11f9e9c8bae46e83ae4303f15
Pirer24 <pirer23@gmail.com>
2021-02-03 20:07:55 +0100

    Fix swagger

--- infrastructure/src/main/java/app/majime/infrastructure/lims/LimsApplication.java
+++ infrastructure/src/main/java/app/majime/infrastructure/lims/LimsApplication.java
@@ -3,11 +3,13 @@ package app.majime.infrastructure.lims;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EntityScan("app.majime")
+@Configuration
 @EnableSwagger2
 public class LimsApplication {
 
