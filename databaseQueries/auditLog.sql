a7c5882ed63f57fe3d0a357e7964cee23a17279f
s16600 <61659844+s16600@users.noreply.github.com>
2021-02-18 22:42:10 +0100

    Added auditLog for logging changes and triggers for logged tables.

---
+++ databaseQueries/auditLog.sql
@@ -0,0 +1,94 @@
+DROP TABLE audit_log;
+DROP TYPE dml_type;
+
+CREATE TYPE dml_type AS ENUM ('INSERT', 'UPDATE', 'DELETE');
+
+CREATE TABLE IF NOT EXISTS audit_log (
+                                         id SERIAL PRIMARY KEY,
+                                         tab_name varchar(255) NOT NULL,
+    element_id bigint NOT NULL,
+    old_row_data jsonb,
+    new_row_data jsonb,
+    dml_type dml_type NOT NULL,
+    dml_timestamp timestamp NOT NULL,
+    dml_created_by varchar(255) NOT NULL,
+    dml_reason varchar(255) NOT NULL
+    );
+
+CREATE OR REPLACE FUNCTION audit_trigger_func()
+RETURNS trigger AS $body$
+BEGIN
+   if (TG_OP = 'INSERT') then
+       INSERT INTO audit_log (
+			tab_name,
+    		element_id,
+    		old_row_data,
+		   	new_row_data,
+    		dml_type,
+    		dml_timestamp,
+    		dml_created_by,
+			dml_reason
+       )
+       VALUES(
+           TG_RELNAME,
+		   NEW.id,
+           null,
+           to_jsonb(NEW),
+           'INSERT',
+           CURRENT_TIMESTAMP,
+           CURRENT_USER,
+		   'New record'
+       );
+RETURN NEW;
+
+elsif (TG_OP = 'UPDATE') then
+       INSERT INTO audit_log (
+			tab_name,
+    		element_id,
+    		old_row_data,
+		   	new_row_data,
+    		dml_type,
+    		dml_timestamp,
+    		dml_created_by,
+			dml_reason
+       )
+       VALUES(
+           TG_RELNAME,
+		   NEW.id,
+           to_jsonb(OLD),
+           to_jsonb(NEW),
+           'UPDATE',
+           CURRENT_TIMESTAMP,
+           CURRENT_USER,
+		   'Updated record'
+       );
+
+RETURN NEW;
+elsif (TG_OP = 'DELETE') then
+       INSERT INTO audit_log (
+			tab_name,
+    		element_id,
+    		old_row_data,
+		   	new_row_data,
+    		dml_type,
+    		dml_timestamp,
+    		dml_created_by,
+			dml_reason
+       )
+       VALUES(
+           TG_RELNAME,
+		   OLD.id,
+           to_jsonb(OLD),
+           null,
+           'DELETE',
+           CURRENT_TIMESTAMP,
+           CURRENT_USER,
+		   'Deleted record'
+       );
+
+RETURN OLD;
+end if;
+
+END;
+$body$
+LANGUAGE plpgsql
---
+++ databaseQueries/triggers.sql
@@ -0,0 +1,105 @@
+DROP TRIGGER address_audit_trigger ON address;
+DROP TRIGGER batch_audit_trigger ON batch;
+DROP TRIGGER certificate_audit_trigger ON certificate;
+DROP TRIGGER dictionary_audit_trigger ON dictionary;
+DROP TRIGGER lab_audit_trigger ON lab;
+DROP TRIGGER manufacturer_audit_trigger ON manufacturer;
+DROP TRIGGER material_audit_trigger ON material;
+DROP TRIGGER outofspec_audit_trigger ON outofspec;
+DROP TRIGGER parameter_audit_trigger ON parameter;
+DROP TRIGGER permission_audit_trigger ON permission;
+DROP TRIGGER result_audit_trigger ON result;
+DROP TRIGGER result_status_history_audit_trigger ON result_status_history;
+DROP TRIGGER role_audit_trigger ON role;
+DROP TRIGGER role_permission_audit_trigger ON role_permission;
+DROP TRIGGER sample_audit_trigger ON sample;
+DROP TRIGGER sample_lab_audit_trigger ON sample_lab;
+DROP TRIGGER sample_status_history_audit_trigger ON sample_status_history;
+DROP TRIGGER specification_audit_trigger ON specification;
+DROP TRIGGER specification_status_history_audit_trigger ON specification_status_history;
+DROP TRIGGER supplier_audit_trigger ON supplier;
+DROP TRIGGER unit_audit_trigger ON unit;
+
+CREATE TRIGGER address_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON address
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER batch_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON batch
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER certificate_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON certificate
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER dictionary_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON dictionary
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER lab_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON lab
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER manufacturer_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON manufacturer
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER material_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON material
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER outofspec_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON outofspec
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER parameter_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON parameter
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER permission_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON permission
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER result_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON result
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER result_status_history_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON result_status_history
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER role_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON role
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER role_permission_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON role_permission
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER sample_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON sample
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER sample_lab_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON sample_lab
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER sample_status_history_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON sample_status_history
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER specification_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON specification
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER specification_status_history_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON specification_status_history
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER supplier_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON supplier
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
+
+CREATE TRIGGER unit_audit_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON unit
+    FOR EACH ROW EXECUTE FUNCTION audit_trigger_func();
