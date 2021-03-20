70a727e7901ba4b37b6aed5e2c41b81fd752a595
Pirer24 <pirer23@gmail.com>
2021-03-20 22:25:13 +0100

    rm usecase sample

--- src/main/java/app/majime/lims/sample/usecase/SampleOperations.java
+++
@@ -1,9 +0,0 @@
-package app.majime.lims.sample.usecase;
-
-import app.majime.lims.sampleLab.SampleLab;
-
-public interface SampleOperations {
-
-    void addSampleLab(SampleLab sampleLab);
-    void removeSampleLab(SampleLab sampleLab);
-}
