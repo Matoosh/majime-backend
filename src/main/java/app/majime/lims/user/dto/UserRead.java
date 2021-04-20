a373612586325c1498ef6fb4de44710684a1df17
Mateusz Niełacny <19360197+Matoosh@users.noreply.github.com>
2021-04-20 09:42:38 +0200

    Changed class name from UserDto to UserRead and moved to dto package. Deleted password var.

--- src/main/java/app/majime/lims/user/dto/UserRead.java
+++ src/main/java/app/majime/lims/user/dto/UserRead.java
@@ -1,19 +1,21 @@
-package app.majime.lims.user;
+package app.majime.lims.user.dto;
 
+import app.majime.lims.user.Role;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Collection;
+
 @Getter
 @Setter
 @Builder
-class UserDto {
+public class UserRead {
     private String firstName;
     private String lastName;
     private String username;
-    private String password;
     private String email;
     private String phone;
     private String deleted;
-    private Role role;
+    private Collection<Role> roles;
 }
