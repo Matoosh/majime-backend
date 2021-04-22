84e364535c73d229ca61abd4ba576deb87c6cd9a
Mateusz Niełacny <19360197+Matoosh@users.noreply.github.com>
2021-04-22 21:05:15 +0200

    +changed to have permissions in spring security context instead roles. We can use permission on endpoints now +Values can't be initialed if we want get values from properties file - changed.

--- src/main/java/app/majime/lims/security/MyUserDetails.java
+++ src/main/java/app/majime/lims/security/MyUserDetails.java
@@ -1,12 +1,19 @@
 package app.majime.lims.security;
 
+import app.majime.lims.role.Permission;
+import app.majime.lims.role.Role;
 import app.majime.lims.user.User;
 import app.majime.lims.user.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import static java.lang.String.format;
@@ -31,11 +38,36 @@ public class MyUserDetails implements UserDetailsService {
         return org.springframework.security.core.userdetails.User
                 .withUsername(email)
                 .password(user.get().getPassword())
-                .authorities(user.get().getRole().getName())
+                .authorities(getAuthorities(user.get().getRoles()))
                 .accountExpired(false)
                 .accountLocked(false)
                 .credentialsExpired(false)
                 .disabled(false)
                 .build();
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
+        return getGrantedAuthorities(getPrivileges(roles));
+    }
+
+    private List<String> getPrivileges(Collection<Role> roles) {
+
+        List<String> privileges = new ArrayList<>();
+        List<Permission> collection = new ArrayList<>();
+        for (Role role : roles) {
+            collection.addAll(role.getPermissions());
+        }
+        for (Permission item : collection) {
+            privileges.add(item.getCode());
+        }
+        return privileges;
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (String privilege : privileges) {
+            authorities.add(new SimpleGrantedAuthority(privilege));
+        }
+        return authorities;
     }
 }
