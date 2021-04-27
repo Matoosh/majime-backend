835c50d0a57c2d9297a27a1693e900adf5dfb1c8
Maciek <s17245@pjwstk.edu.pl>
2021-04-27 19:09:45 +0100

    Add result, add public in Sample, check it please

---
+++ src/main/java/app/majime/lims/result/ResultDto.java
@@ -0,0 +1,22 @@
+package app.majime.lims.result;
+
+import app.majime.lims.parameter.ParameterDto;
+import app.majime.lims.sample.SampleDto;
+import app.majime.lims.utils.StatusDeleted;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ResultDto {
+
+    Long id;
+    String value;
+    StatusDeleted deleted;
+    ResultStatus status;
+    SampleDto sample;
+    ParameterDto parameter;
+
+}
---
+++ src/main/java/app/majime/lims/result/ResultService.java
@@ -0,0 +1,64 @@
+package app.majime.lims.result;
+
+import app.majime.lims.utils.StatusDeleted;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+class ResultService {
+
+    private final ResultRepository resultRepository;
+
+    List<Result> findAll() {
+        return resultRepository.findAll();
+    }
+
+    Optional<Result> findById(Long id) {
+        return resultRepository.findById(id);
+    }
+
+    Result create(Result result) {
+        result.setDeleted(StatusDeleted.FALSE);
+        result.setStatus(ResultStatus.NEW);
+        return resultRepository.save(result);
+    }
+
+    Result updateResult(Long id, ResultDto resultDto) throws EntityNotFoundException {
+        Optional<Result> resultOptional = resultRepository.findById(id);
+
+        if (!resultOptional.isPresent()){
+            throw new EntityNotFoundException("Not found result id = " + id);
+        }
+        Result result = resultOptional.get();
+        result = result.buildFrom(resultDto);
+        return resultRepository.save(result);
+    }
+
+    Result updateStatus(Long id, ResultStatus newStatus) throws EntityNotFoundException {
+        Optional<Result> resultOptional = resultRepository.findById(id);
+
+        if (resultOptional.isPresent()) {
+            Result result = resultOptional.get();
+            result.setStatus(newStatus);
+            return resultRepository.save(result);
+        }
+        throw new EntityNotFoundException("Not found Result id = " + id);
+    }
+
+    Result deleteResult(Long id) throws EntityNotFoundException {
+        Optional<Result> resultOptional = resultRepository.findById(id);
+
+        if (resultOptional.isPresent()){
+            Result resultDb = resultOptional.get();
+            resultDb.setDeleted(StatusDeleted.TRUE);
+            return resultRepository.save(resultDb);
+        }
+        throw new EntityNotFoundException("Not found Result id = " + id);
+    }
+
+}
