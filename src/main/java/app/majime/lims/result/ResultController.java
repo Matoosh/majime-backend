a6b33f98ea162564b973b1af8bcf56415e3dc578
Maciek <s17245@pjwstk.edu.pl>
2021-04-29 22:10:22 +0100

    Rename getResultBySampleId, by request

--- src/main/java/app/majime/lims/result/ResultController.java
+++ src/main/java/app/majime/lims/result/ResultController.java
@@ -66,7 +66,7 @@ class ResultController {
         }
     }
 
-    @GetMapping("/result/{id}")
+    @GetMapping("/result/sample/{id}")
     List<ResultDto> getResultBySampleId(@PathVariable(value = "id") Long id) {
         List <Result> resultList = resultService.findBySampleId(id);
         return resultList
