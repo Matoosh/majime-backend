36b2d49307598956101144b834fe6e0d49374507
Pirer24 <pirer23@gmail.com>
2021-05-16 16:11:24 +0200

    check status

--- src/main/java/app/majime/lims/result/ResultService.java
+++ src/main/java/app/majime/lims/result/ResultService.java
@@ -1,7 +1,13 @@
 package app.majime.lims.result;
 
+import app.majime.lims.exception.CustomException;
+import app.majime.lims.parameter.ParameterService;
+import app.majime.lims.sample.Sample;
+import app.majime.lims.sample.SampleRepository;
+import app.majime.lims.sample.SampleStatus;
 import app.majime.lims.utils.StatusDeleted;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -14,6 +20,8 @@ import java.util.stream.Collectors;
 class ResultService {
 
     private final ResultRepository resultRepository;
+    private final SampleRepository sampleRepository;
+    private final ParameterService parameterService;
 
     List<Result> findAll() {
         return resultRepository.findAll();
@@ -25,7 +33,17 @@ class ResultService {
 
     Result create(Result result) {
         result.setDeleted(StatusDeleted.FALSE);
-        result.setStatus(ResultStatus.NEW);
+        Sample sample = result.getSample();
+        if (result.getStatus() == ResultStatus.OOS && sample.getStatus() != SampleStatus.OOS) {
+            sample.setDeleted(StatusDeleted.FALSE);
+            sample.setStatus(SampleStatus.OOS);
+            try {
+                sampleRepository.save(sample);
+            } catch (CustomException customException){
+                throw new CustomException("Error with change status in sample", HttpStatus.UNPROCESSABLE_ENTITY);
+            }
+        }
+        checkAllResultsFiled(result);
         return resultRepository.save(result);
     }
 
@@ -37,7 +55,17 @@ class ResultService {
         }
         Result result = resultOptional.get();
         result = result.buildFrom(resultDto);
-        result.setDeleted(StatusDeleted.FALSE);
+        Sample sample = result.getSample();
+        if (result.getStatus() == ResultStatus.OOS && sample.getStatus() != SampleStatus.OOS) {
+            sample.setDeleted(StatusDeleted.FALSE);
+            sample.setStatus(SampleStatus.OOS);
+            try {
+                sampleRepository.save(sample);
+            } catch (CustomException customException){
+                throw new CustomException("Error with change status in sample", HttpStatus.UNPROCESSABLE_ENTITY);
+            }
+        }
+        checkAllResultsFiled(result);
         return resultRepository.save(result);
     }
 
@@ -79,4 +107,23 @@ class ResultService {
         throw new EntityNotFoundException("Result not found: " + id);
     }
 
+    void checkAllResultsFiled(Result result) throws EntityNotFoundException{
+        int numberOfResults = findBySampleId(result.getSample().getId()).stream()
+                .filter(resultOb -> resultOb.getStatus() == ResultStatus.OOSADDED || resultOb.getStatus() == ResultStatus.ENTERED)
+                .collect(Collectors.toList()).size()
+                + ((result.getId() == null && result.getStatus() == ResultStatus.ENTERED) ? 1 : 0);
+        int numberOfParameters = parameterService.findBySpecificationId(result.getParameter().getSpecification().getId())
+                .stream().filter(parameter -> (parameter.getSpecification().getId() == result.getSample().getSpecification().getId()))
+                .collect(Collectors.toList()).size();
+        if(numberOfResults == numberOfParameters){
+            Sample sample = result.getSample();
+            sample.setDeleted(StatusDeleted.FALSE);
+            sample.setStatus(SampleStatus.CHECK);
+            try {
+                sampleRepository.save(sample);
+            } catch (CustomException customException){
+                throw new CustomException("Error with change status in sample", HttpStatus.UNPROCESSABLE_ENTITY);
+            }
+        }
+    }
 }
