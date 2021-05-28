e0947011c1f1a83825b0b7810b44dddfed976d3c
s16600 <61659844+s16600@users.noreply.github.com>
2021-05-28 19:54:38 +0200

    Charts again corrected for a new statuses

--- src/main/java/app/majime/lims/chart/SampleChartController.java
+++ src/main/java/app/majime/lims/chart/SampleChartController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.List;
 import static java.util.stream.Collectors.toList;
 
