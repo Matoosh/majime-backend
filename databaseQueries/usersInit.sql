f8d8fc5e0bfcbf35c6aa4b08e97a8d7a903064d1
s16600 <61659844+s16600@users.noreply.github.com>
2021-06-06 21:08:33 +0200

    Solved sequences problem

--- databaseQueries/usersInit.sql
+++ databaseQueries/usersInit.sql
@@ -17,8 +17,9 @@ INSERT INTO public.permission (id, code, description, name) VALUES (11, 'REPORTS
 INSERT INTO public.permission (id, code, description, name) VALUES (12, 'LAB', 'Uprawnienie dające dostęp do podstrony Laboratoria oraz operacji wykonywanych w tle', 'Laboratoria');
 INSERT INTO public.permission (id, code, description, name) VALUES (13, 'SPECIFICATION', 'Uprawnienie dające dostęp do podstrony Specyfikacje oraz operacji wykonywanych w tle', 'Specyfikacje');
 INSERT INTO public.permission (id, code, description, name) VALUES (14, 'MATERIAL', 'Uprawnienie dające dostęp do podstrony Materiały oraz operacji wykonywanych w tle', 'Materiały');
-INSERT INTO public.permission (id, code, description, name) VALUES (15, 'SUPPLIER ', 'Uprawnienie dające dostęp do podstrony Dostawcy oraz operacji wykonywanych w tle', 'Dostawcy');
+INSERT INTO public.permission (id, code, description, name) VALUES (15, 'SUPPLIER', 'Uprawnienie dające dostęp do podstrony Dostawcy oraz operacji wykonywanych w tle', 'Dostawcy');
 INSERT INTO public.permission (id, code, description, name) VALUES (16, 'MANUFACTURER', 'Uprawnienie dające dostęp do podstrony Producenci oraz operacji wykonywanych w tle', 'Producenci');
+SELECT setval('permission_id_seq',((SELECT COUNT(*) FROM permission) + 1));
 --role
 --TRUNCATE TABLE public.role CASCADE;
 INSERT INTO public.role (id, deleted, name) VALUES (1, 'FALSE', 'Administrator');
@@ -28,6 +29,7 @@ INSERT INTO public.role (id, deleted, name) VALUES (4, 'FALSE', 'Kierownik labor
 INSERT INTO public.role (id, deleted, name) VALUES (5, 'FALSE', 'Próbkarz');
 INSERT INTO public.role (id, deleted, name) VALUES (6, 'FALSE', 'Laborant');
 INSERT INTO public.role (id, deleted, name) VALUES (7, 'FALSE', 'Analityk');
+SELECT setval('role_id_seq',((SELECT COUNT(*) FROM role) + 1));
 --role_permission
 --TRUNCATE TABLE public.role_permission CASCADE;
 INSERT INTO public.role_permission (role_id, permission_id) VALUES (1, 1);
@@ -76,7 +78,6 @@ INSERT INTO public.address (id, apartment_number, city, country_code, created_by
 TRUNCATE TABLE public.lab CASCADE;
 INSERT INTO public.lab (id, created_by, deleted, name, reason, address_id) VALUES (1, null, 'FALSE', 'Centrum', null, 1);
 --lab_user
-
  */
 --TRUNCATE TABLE public.lab_user CASCADE;
 INSERT INTO public.lab_user (id, created_by, deleted, email, first_name, last_name, password, phone, reason, lab_id) VALUES (1, null, 'FALSE', 'administracja@majime.app', 'Jan', 'Brzechwa', '$2a$12$yNlbhpJlmPpMasc4Wh06IepaPRP6Mr6gtgMyln86E.Y1NyljuMlRC', '', null, 1);
